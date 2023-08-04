1e8060f10325bc9168f5024cfd3886ee9307bfc6
Chinmay Jyotishi <chinmayjyotishi111@gmail.com>
2023-08-05 02:26:10 +0530

    first commit

---
+++ 01_First.py
@@ -0,0 +1,24 @@
+# # how to print a statement in python
+
+# print('hii how are you ')
+
+# print("good morning")
+
+# print(''' hii how are you''')
+
+
+# a=10
+# print(a)
+# print('a')
+
+# var=45464
+# print(var)
+
+
+c=5454
+print(c)
+
+a=44
+b=20
+c=a+b
+print(c)
---
+++ 02_variable.py
@@ -0,0 +1,84 @@
+# 5 rules of declaring variable
+
+# 1-- variable name should be started with only alphabets and _ (underscore)
+
+# abc=10
+# print(abc)
+
+# abs=54
+# print(abs)
+
+# name='manisha'
+# print(name)
+
+# _name='kk'
+# print(_name)
+
+# name_="ab"
+# print(name_)
+
+# 2-- digits can be used as variable name but the name should be started with 1st rule 
+
+# java10=45
+# print(java10)
+
+# college45='nit'
+# print(college45)
+
+# college_01="Nalanda institute of technology"
+# print(college_01)
+
+# name="sumit"
+# Name="amit"
+# name="cj"  #value updation
+# print(name)
+# print(Name)
+
+# 3-- no white space can be used
+
+# var 01="hello"
+# print(var 01)
+
+# var_01="hello"
+# print(var_01)
+
+# College Name="IIT institute"
+# print(College Name)
+
+# CollegeName="IIT institute"
+# print(CollegeName)
+
+
+# 4-- only _ can be used as special charecter
+
+
+# name_="suresh"
+# print(name_)
+
+# name@="mahesh"
+# print(name@)
+
+# name$="daa"
+# print(name$)
+
+
+
+# 5-- keyword cant be used as variable name
+# if ="name"
+# print(if)
+
+If="name"
+print(If)
+
+# while="hello"
+# print(while)
+
+While="hello"
+print(While)
+
+
+# import keyword
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+
+
