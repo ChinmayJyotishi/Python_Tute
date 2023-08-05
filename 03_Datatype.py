3fdb576b0d783d1271573ab896d5adf71bb51193
Chinmay Jyotishi <chinmayjyotishi111@gmail.com>
2023-08-06 02:13:11 +0530

    file

---
+++ 03_Datatype.py
@@ -0,0 +1,48 @@
+# number=10
+# print(number)
+
+# datatypes
+# 1-integer-- int 
+# 2-float
+# 3-boolean
+# 4-complex 
+# 5- string
+
+# integer
+# number=10
+# print(number)
+# print(type(number))
+
+# a=10
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+
+# float
+# abc=10.5
+# print(abc)
+# print(type(abc))
+
+# num=45.5555555
+# print(num)
+
+num=10e4
+print(num)
+
+# complex 
+comp=45+5j
+print(comp)
+
+comp1=45+5j
+comp2=56+8j
+print(comp1+comp2)
+
+
+a=154+2j
+print(a.real)
+print(a.imag)
+
+
---
+++ 04_String.py
@@ -0,0 +1,31 @@
+# string is the collection of number of characters 
+
+# name='ashok raj'
+# name1="ashok raj"
+# name2='''ashok raj'''
+
+# print(name)
+# print(name1)
+# print(name2)
+
+
+# That is my father's car 
+# line='That is my father's car'
+# print(line)
+
+# line2="That is my father's car "
+# print(line2)
+
+# "tome mote rakta dia mu tamaku swadhinata debi"
+
+quote="tome mote rakta dia mu tamaku swadhinata debi"
+print(quote)
+
+quote1=''' "tome mote rakta dia mu tamaku swadhinata debi" '''
+print(quote1)
+
+address='''lane-11,
+khandagiri,
+bbsr'''
+print(address)
+
